3fa01c765271fb2a5e899e107972c53079298e87
eroshevichav <guznastusha@tut.by>
2022-09-15 16:56:43 +0300

    ok

---
+++ 4_2.py
@@ -0,0 +1,10 @@
+# Без использования collections, написать программу, которая будет создавать словарь для
+# подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
+
+text = input()
+а = text[]
+letters = {for a in range(0, len(text))}
+total = text.count(a, 0, len(text)-1)
+result ={'letters': 'total'}
+print(result.items)
+
